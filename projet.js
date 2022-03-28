e6e5530a88bed70d3d07123e248803e544d6fdd3
Simon <sissisix@hotmail.fr>
2022-03-28 21:45:02 +0200

    Del fichier inutils, contenu modal ok !

---
+++ projet.js
@@ -0,0 +1,171 @@
+
+var proj = {
+    "maj": "2022_03_28",
+    "projets": [
+        {
+            "name": "L'équipe Explore",
+            "date": "",
+            "state": "",
+            "description": "Développement d'un site web pour une équipe d'étudiants en journalisme, pour le prix L'équipe Explore : 1ère place",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_EquipeExplore.png",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        },
+        {
+            "name": "Youstiti",
+            "date": "",
+            "state": "",
+            "description": "Développement d’une application Android et iOS dans le cadre d’un stage de fin d’étude pour la startup Youstiti. Elle permet de géolocaliser l’utilisateur, et avec ces données, des caméras sont orientés pour filmer l’utilisateur durant ses loisirs. Pour cette application, j’ai implémenter des designs, ainsi que plusieurs fonctionnalités, tel que la géolocalisation, le changement de langue, l’export de photos/vidéos sur un serveur. J’ai également pu développer différentes communications avec le serveur, comme pour la demande de connexion, en utilisant des sockets ssl.",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_Youstiti.png",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        },
+        {
+            "name": "ULR Loc'Salle",
+            "date": "",
+            "state": "",
+            "description": "Développement d’une application Android pour un cours de gestion de projet, qui permet de localiser des salles et des bâtiments, mais aussi de pouvoir savoir quelles salles sont libres pour travailler. Mais toutes les fonctionnalités n’étant pas finies lors du rendu, je continue toujours de la développer.",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_Ulr.png",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        },
+        {
+            "name": "Snake",
+            "date": "",
+            "state": "",
+            "description": "Création d’un jeu mobile Android snake, plus le niveau augmente, plus le serpent va vite, plus la zone de jeu rétrécie et plus le score à obtenir pour finir le niveau est élevé.",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_Snake.png",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        },
+        {
+            "name": "Tetris",
+            "date": "",
+            "state": "",
+            "description": "Création d’un jeu tetris dans le cadre d’une game jam organisé par Lola Guilldou, Miss Myu et l’Agrume Masqué. Jeu réalisé seul en un week end en utilisant le logiciel Construct 2. J’ai préféré partir sur un jeu simple mais qui m’a permit d’approfondir les bases de Construct 2, ainsi que certains concepts de programmation. Surtout qu’étant seul j’aurais eu du mal à faire un scénario, des graphismes et programmer le jeu, de plus c’était ma première expérience.",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_Tetris",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        },
+        {
+            "name": "Hamster Life",
+            "date": "",
+            "state": "",
+            "description": "Création d’un jeu qui simule la vie d’un hamster pour un projet personnel de calendrier de l’avent sous Construct 2. Chaque événement de sa vie consiste en un mini-jeu, boire consiste à sauter de bulle en bulle, dormir en un simple jeu de type memory, pour manger il faut récupérer de la nourriture qui tombe du ciel, et courir dans la roue est un jeu de plateforme classique.",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_Hamster",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        },
+        {
+            "name": "Chifoumi",
+            "date": "",
+            "state": "",
+            "description": "Création d’un jeu de pierre - feuille - ciseaux dans le cadre d’un cours d’interface homme machine. Le but était de nous introduire au problème de la redimension de fenêtre et à wx-widgets, tout en nous faisant créer un projet complet en C++. Ce projet m’aura permit de comprendre que le redimensionnement de fenêtre est quelque chose de primordial pour un logiciel, et que l’ergonomie en dépend. Au moment de la création de ce jeu, j’avais choisi de définir une taille de fenêtre par défaut, et d’empêcher tout redimensionnement. Aujourd’hui, avec du recul est de nouvelle expérience, je pense que j’aurai plutôt dû autoriser le redimensionnement, en gardant les images et les textes centrés mais s’éloignant/se rapprochant du centre.",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_PvsC",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        },
+        {
+            "name": "Slot Machine",
+            "date": "",
+            "state": "",
+            "description": "Création d’une machine à sous dans le cadre d’un cours de programmation événementielle en python, ce qui nous a permit d’apprendre à utiliser PyQt5 ainsi que QtCreator.",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_Chance",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        },
+        {
+            "name": "Light-minded",
+            "date": "",
+            "state": "",
+            "description": "Développement du jeu \"Light-minded\" sur Unity pour la Global Game Jam de 2021, sur le thème \"Lost & Found\". Ce jeu se base sur l'obscurité et l'utilisation d'une lumière légère pour se déplacer et résoudre des puzzles. Nous avons donc dû créer un système de gestion de la lumière permettant de, par exemple, charger un générateur ou encore créer un rayon lumineux.",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_Light",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": "https://globalgamejam.org/2021/games/light-minded-6"
+        },
+        {
+            "name": "Run A Round",
+            "date": "",
+            "state": "",
+            "description": "Création d'un Runner sous Unity permettant de tourner en rond alors que le nombre d'obstacle augmente au fur et à mesure",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_Run",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        },
+        {
+            "name": "Spaaaaace",
+            "date": "",
+            "state": "",
+            "description": "Développement d'un jeu de course spatiale sur Unity. J'ai dû développer un système d'apparition automatique de divers objets spatiaux qui peuvent gêner le joueur dans sa course ainsi que d'endommager son vaisseau. De plus créer un système d'accélération qui diffère selon le vaisseau utilisé. Un système d'arme et de tir a aussi été rajouté pour pouvoir détruire différents obstacles.",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_Space",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        },
+        {
+            "name": "Metro",
+            "date": "",
+            "state": "",
+            "description": "Développement d’un logiciel en python permettant de calculer le chemin le plus court entre deux stations du métro de Paris. Pour cela j'ai utilisé plusieurs méthodes. Tout d'abord l'algorithme de Dijkstra classique, puis une amélioration qui évite des redondances de recherche en évitant de revenir plusieurs fois sur une même station. Je l'ai aussi fait en utilisant l'algorithme A* ainsi qu'en démarrant Dijkstra dans les deux sens. En appliquant ces diverses méthodes, on peut remarquer que l'algorithme des A* et celui du double Dijkstra sont les plus efficaces, du fait d'une complexité moindre.",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_Metro",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        },
+        {
+            "name": "Marching Square",
+            "date": "",
+            "state": "",
+            "description": "Développement d'un logiciel en C, permettant de mettre en place l'algorithme de \"Marching Square\". Cet algorithme permet de visualiser des données sous forme d'images 2d. Dans le cas ci dessous, les données utilisées correspondaient à celle d'un relief montagneux.",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_Square",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        },
+        {
+            "name": "Météo",
+            "date": "",
+            "state": "",
+            "description": "Développement d'un logiciel en C, permettant de visualiser des données correspondant à des champs de vecteurs. Pour cela un maillage est défini, puis selon un fichier de données, des vecteurs sont placés, contenant une vitesse. Ce qui nous donne, pour l'exemple ci dessus, la modélisation des courants d'air de la zone définie par les données.",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_Vent",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        },
+        {
+            "name": "EDF Flower",
+            "date": "",
+            "state": "",
+            "description": "Création d'une animation sur Blender, devant montrer l'évolution de la smart flower d'EDF au cours de la journée",
+            "imgUrl": "",
+            "imgIcUrl": "Icn_FleurEdf",
+            "vidUrl": "",
+            "gifUrl": "",
+            "footer": ""
+        }
+    ]
+}
